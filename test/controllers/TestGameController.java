e5eff36e8fffe53663657695a34629aeb5772793
40099112 <rurquhart01@qub.ac.uk>
2024-04-05 13:12:03 +0100

    Added in functionality for players to select an avatar, I also added a test for the listFilesInDir method added for this functionality

--- test/controllers/TestGameController.java
+++ test/controllers/TestGameController.java
@@ -1,11 +1,73 @@
-/**
-* @author : Ryan Urquhart
-* @studentNumber : 40099112
-* @email : rurquhart@qub.ac.uk
-* @created : Mar 31, 2024 - 5:36:44 PM
-**/
 package controllers;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import models.Board;
+import models.EcoZoneSquare;
+import models.GoSquare;
+import models.InvestmentSquare;
+import models.Player;
+import models.Square;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestGameController {
 
+    private String dir1, dir2, dir3, dir4;
+    private List<String> dir1Expected, dir2Expected, dir3Expected, dir4Expected;
+    private List<String> dir1Actual, dir2Actual, dir3Actual, dir4Actual;
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeEach
+    void setUp() throws Exception {
+        dir1 = System.getProperty("user.dir") + "/src/resources";
+        dir2 = System.getProperty("user.dir") + "/";
+        dir3 = System.getProperty("user.dir") + "/src/main";
+        dir4 = System.getProperty("user.dir") + "/src/models";
+        dir1Expected = Arrays
+                .asList(new String[] { "asciititle.txt", "cloudavatar.txt", "ligthningavatar.txt", "sunavatar.txt",
+                        "treeavatar.txt", "waveavatar.txt" });
+        dir2Expected = Arrays.asList(new String[] { ".gitlab-ci.yml", "src", ".gitignore", ".project", "README.md",
+                ".metadata", ".git", ".settings", "whileScript.sh", "test", "bin", ".classpath" });
+        dir3Expected = Arrays.asList(new String[] { "Main.java" });
+        dir4Expected = Arrays.asList(new String[] { "Board.java", "EcoZoneSquare.java", "GoSquare.java",
+                "InvestmentSquare.java", "Player.java", "Square.java" });
+
+    }
+
+    /**
+     * Tests 'listFilesInDir' from 'GameController', if more files are added in the
+     * tested directory they need added into this test
+     * 
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    @Test
+    void testListFilesInDir() throws IOException, URISyntaxException {
+        dir1Actual = GameController.listFilesInDir(dir1, "");
+        dir2Actual = GameController.listFilesInDir(dir2, "");
+        dir3Actual = GameController.listFilesInDir(dir3, "");
+        dir4Actual = GameController.listFilesInDir(dir4, "");
+        Collections.sort(dir1Expected);
+        Collections.sort(dir2Expected);
+        Collections.sort(dir3Expected);
+        Collections.sort(dir4Expected);
+        Collections.sort(dir1Actual);
+        Collections.sort(dir2Actual);
+        Collections.sort(dir3Actual);
+        Collections.sort(dir4Actual);
+        assertEquals(dir1Expected, dir1Actual);
+        assertEquals(dir2Expected, dir2Actual);
+        assertEquals(dir3Expected, dir3Actual);
+        assertEquals(dir4Expected, dir4Actual);
+    }
 }
