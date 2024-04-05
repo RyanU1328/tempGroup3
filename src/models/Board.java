96b24af6e0745bc02d71aed39419ed12c097b4b2
40099112 <rurquhart01@qub.ac.uk>
2024-04-05 16:34:08 +0100

    modified board.java to work with new investment square constructor

--- src/models/Board.java
+++ src/models/Board.java
@@ -8,17 +8,17 @@ public class Board {
 
     public Board() {
         squares.add(new GoSquare("Go"));
-        squares.add(new InvestmentSquare("Sunny Acres", 20, 10)); 
-        squares.add(new InvestmentSquare("Radiant Ridge", 30, 15));
-        squares.add(new InvestmentSquare("Windy Mills", 50, 25)); 
-        squares.add(new InvestmentSquare("Gusto Grove", 60, 30));
-        squares.add(new InvestmentSquare("Airy Heights", 70, 35)); 
-        squares.add(new EcoZoneSquare("Eco Zone")); 
-        squares.add(new InvestmentSquare("Splashviille", 90, 45));
-        squares.add(new InvestmentSquare("Torrential Terrace", 100, 50)); 
-        squares.add(new InvestmentSquare("Hydro Haven", 110, 55));
-        squares.add(new InvestmentSquare("Nucleo nest", 130, 65)); 
-        squares.add(new InvestmentSquare("Atomic Oasis", 140, 70));
+        squares.add(new InvestmentSquare("Sunny Acres", 20));
+        squares.add(new InvestmentSquare("Radiant Ridge", 30));
+        squares.add(new InvestmentSquare("Windy Mills", 50));
+        squares.add(new InvestmentSquare("Gusto Grove", 60));
+        squares.add(new InvestmentSquare("Airy Heights", 70));
+        squares.add(new EcoZoneSquare("Eco Zone"));
+        squares.add(new InvestmentSquare("Splashviille", 90));
+        squares.add(new InvestmentSquare("Torrential Terrace", 100));
+        squares.add(new InvestmentSquare("Hydro Haven", 110));
+        squares.add(new InvestmentSquare("Nucleo nest", 130));
+        squares.add(new InvestmentSquare("Atomic Oasis", 140));
 
     }
 
@@ -27,6 +27,6 @@ public class Board {
     }
 
     public int getSize() {
-        return squares.size(); 
+        return squares.size();
     }
 }
